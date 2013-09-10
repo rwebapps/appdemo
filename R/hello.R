ca247bf514d9b86a701d973173e0b007af5aefef
Jeroen Ooms <jeroenooms@gmail.com>
2013-09-10 13:42:54 -0700

    first commit

---
+++ R/hello.R
@@ -0,0 +1,14 @@
+#' Hello World
+#' 
+#' Basic hello world function to be called from the demo app
+#' 
+#' @export
+#' @param myname your name. Required.
+hello <- function(myname = ""){
+  if(myname == ""){
+    stop("Tell me your name!")
+  }
+  list(
+    message = paste("hello", myname, "!")
+  )
+}
