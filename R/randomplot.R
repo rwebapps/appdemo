c9088a98c6a7c008f7f67432036c82af32199f99
Jeroen Ooms <jeroenooms@gmail.com>
2013-09-10 14:30:45 -0700

    cleanup

---
+++ R/randomplot.R
@@ -0,0 +1,23 @@
+#' Make a random plot
+#' 
+#' This function creates a random histogram plot.
+#' 
+#' @export
+#' @param n numer of random values 
+#' @param dist one of "norm" or "unif".
+randomplot <- function(n, dist=c("normal", "uniform")){
+  #input validation
+  dist <- match.arg(dist)
+  stopifnot(n < 1e6)
+  
+  if(dist == "normal"){
+    hist(rnorm(n))
+  }
+  
+  if(dist == "uniform"){
+    hist(runif(n))
+  }
+  
+  #return nothing
+  invisible();  
+}
