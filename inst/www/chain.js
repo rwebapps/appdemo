e80d0374b69747bce5e9df3529f2b1415e7d12b4
Jeroen Ooms <jeroenooms@gmail.com>
2013-09-13 13:58:42 -0700

    sdf

---
+++ inst/www/chain.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+  
+  function uploadcsv(file, header){
+    //disable the button during upload
+    $("#submitbutton").attr("disabled", "disabled");        
+    
+    //perform the request
+    var req = opencpu.r_fun_call("readcsvnew", {
+      file : file,
+      header : header
+    }, function(session){
+      //on success call printsummary()
+      printsummary(session);
+    });
+    
+    //if R returns an error, alert the error message
+    req.fail(function(){
+      alert("Server error: " + req.responseText);
+    });
+    
+    //after request complete, re-enable the button 
+    req.always(function(){
+      $("#submitbutton").removeAttr("disabled")
+    });        
+  }    
+  
+  
+  function printsummary(mydata){
+    //perform the request
+    var req = opencpu.r_fun_call("printsummary", {
+      mydata : mydata
+    }, function(session){
+      var url = session.getLoc() +  "console/text";
+      downloadfile(url);
+    }).fail(function(){
+      alert("Server error: " + req.responseText);
+    });        
+  }
+  
+  //standard jQuery stuff
+  function downloadfile(url){
+    $.get(url, function(output){
+      $("#output code").text(output)
+    });
+  }
+  
+  $("#submitbutton").on("click", function(){
+    
+    //arguments
+    var myheader = $("#header").val() == "true";
+    var myfile = $("#csvfile")[0].files[0];
+    
+    if(!myfile){
+      alert("No file selected.");
+      return;
+    }
+    
+    uploadcsv(myfile, myheader);        
+  });    
+});
